f2dd40041eef07e172089976e8a6faac4ea850d6
nazzwaputri <nazwa_restyahartanti_34rpl@student.smktelkom-mlg.sch.id>
2025-09-18 11:27:49 +0700

    Add files via upload

---
+++ Bintang.java
@@ -0,0 +1,11 @@
+public class Bintang {
+    public static void main(String[] args) {
+         for (int i = 5; i > 0; i--) {
+            for (int j =1;j<i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    }
+
